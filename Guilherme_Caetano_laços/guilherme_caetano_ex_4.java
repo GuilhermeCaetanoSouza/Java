aabcc999a05551f2fad3618eadb97f5794f31349
Guilherme Caetano <131812809+GuilhermeCaetanoSouza@users.noreply.github.com>
2023-05-29 16:40:16 -0300

    Add files via upload

---
+++ Guilherme_Caetano_laços/guilherme_caetano_ex_4.java
@@ -0,0 +1,17 @@
+/*
+algoritimo para mostrar os numeros pares de 1 a 1000
+Guilherme Caetano de Souza
+29/05
+*/ 
+public class guilherme_caetano_ex_4 {
+    public static void main(String [] args){
+    int cont=0;
+    if(count % 2 ==0){
+        count++
+    }else {
+       System.out.println(cont);
+    cont=cont; 
+    
+    }while(cont<1000);
+}
+}
