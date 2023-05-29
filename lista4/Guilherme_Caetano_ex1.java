77bdddc05b99b1859069af0fa38bf2de400fbe7a
Guilherme Caetano <131812809+GuilhermeCaetanoSouza@users.noreply.github.com>
2023-05-29 17:52:14 -0300

    Add files via upload

---
+++ lista4/Guilherme_Caetano_ex1.java
@@ -0,0 +1,39 @@
+/*Algoritimo que some todos os números cujo quadrado (x²) for divisível por 4.
+Guilherme caetano de souza
+29/05
+*/
+
+public class Taina_Exercicio01{
+
+public static void main(String[] args){
+
+ int lqd = 10; //limite para a soma dos quadrados
+
+ double soma = 0; //variavel para somar
+
+
+
+
+ for(int cont = 1; cont <= lqd; cont++ ){
+//encontrando o quadrado dos valores
+double quadrado = Math.pow(cont, 2); 
+// se o resto de divisão por 4 for igual a 0
+if(quadrado % 4 == 0){ 
+     // some o valor da soma com o valor do quadrado
+soma = soma + Math.pow(cont, 2); 
+
+System.out.println("o quadrado do valor é: " + quadrado);
+
+System.out.println("A soma é:" + soma);
+
+ }else{
+ }
+
+ }
+}
+}
+
+
+
+
+
